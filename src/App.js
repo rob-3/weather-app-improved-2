05b0c159cf06fbba416640d65c11d0f03f689f91
Robert Boyd III <rob@robertcboyd.com>
2021-02-23 20:42:08 -0500

    Add high and low temperatures in output

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
-import { Button, TextField, Box, Card, CardContent } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
 import { useState } from "react";
 
 const searchURL = "https://www.metaweather.com/api/location/search/?query=";
@@ -7,7 +14,7 @@ const weatherURL = "https://www.metaweather.com/api/location/";
 
 function App() {
   const [text, setText] = useState("");
-  const [weather, setWeather] = useState("");
+  const [weatherObj, setWeather] = useState("");
 
   async function fetchCityData() {
     const data = await fetch(searchURL + text).then((blob) => blob.json());
@@ -17,7 +24,9 @@ function App() {
       blob.json()
     );
     const weather = weatherData.consolidated_weather[0].weather_state_name;
-    setWeather(weather);
+    const high = weatherData.consolidated_weather[0].max_temp;
+    const low = weatherData.consolidated_weather[0].min_temp;
+    setWeather({ weather, high, low });
   }
 
   return (
@@ -30,7 +39,11 @@ function App() {
         Click me!
       </Button>
       <Card>
-        <CardContent>{weather}</CardContent>
+        <CardContent>
+          <Typography>{weatherObj.weather}</Typography>
+          <Typography>{weatherObj.high}</Typography>
+          <Typography>{weatherObj.low}</Typography>
+        </CardContent>
       </Card>
     </Box>
   );
